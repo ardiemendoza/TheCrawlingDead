337c103c932f89263c5be2fd1a055b9ad6c8b617
ardiemendoza <ardie_mendoza2005@yahoo.com>
2025-12-05 23:38:47 +0800

    Delete utils directory

--- utils/GameData.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-public class GameData {
-    public static String playerName = "";
-    public static int delay = 50;
-    public static int postDelay = 1000;
-    public static int CheckpointNumber = 0;
-}
